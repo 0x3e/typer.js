c5b8a91e1c5244952c9f54ea89f707b625d8ca21
0x3e <0x3e.com@gmail.com>
2010-11-20 09:54:55 +1100

    changed test words

--- typer.js
+++ typer.js
@@ -5,7 +5,7 @@ function typer(){
   //this.words='this.level=new level(this.display,this.keys,this.words)'
   //this.words='var http = require(\'http\');\nhttp.createServer(function (req, res) {\n  res.writeHead(200, {\'Content-Type\': \'text/plain\'});\n  res.end(\'Hello World\\n\');\n}).listen(8124, "127.0.0.1");\nconsole.log(\'Server running at http://127.0.0.1:8124/\');'
   //this.words='\'"\\~!@#$%^&*|{}[];'
-  this.words='for(i=0;i++;i<100)"'
+  this.words='for(i=0;i++;i<100)'
   //this.words=''
   this.level=new level(this.display,this.keys,this.words)
 	this.update_display=function()
